cb60fbea653836f8c728a73982b852cf8e95c133
ysnblt <106676214+ysnblt@users.noreply.github.com>
2022-09-28 20:43:22 +0300

    Add files via upload

---
+++ PublicvePrivateErişimBelirleyicileri/PublicvePrivateErişimBelirleyicileri/Musteri.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicvePrivateErişimBelirleyicileri
+{
+    public class Musteri
+    {
+        public string isim;
+        public string soyisim;
+        public int maasDegeri;
+        public string cinsiyet;
+        private int yas;
+        public void MusteriBilgileriGoster()
+        {
+            Console.WriteLine("İsmi :" + isim);
+            Console.WriteLine("Soyismi :" + soyisim);
+            Console.WriteLine("Maaşı :" + maasDegeri);
+            Console.WriteLine("Cinsiyeti :" + cinsiyet);
+            Console.WriteLine("Yaşı" + yas);
+        }
+
+        private void İsimSoyisimYazdir(string musteriIsim , string musteriSoyisim)
+        {
+            Console.WriteLine("Müşterinin isim ve soyisimi :" + musteriIsim + " " + musteriSoyisim);
+        }
+
+    }
+}
---
+++ PublicvePrivateErişimBelirleyicileri/PublicvePrivateErişimBelirleyicileri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicvePrivateErişimBelirleyicileri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // public -- private -- protected -- internal
+            Musteri musteri1 = new Musteri();
+            musteri1.isim = "Enes";
+            musteri1.soyisim = "Bayram";
+            musteri1.maasDegeri = 2500;
+            musteri1.yas = 21;
+        }
+    }
+}
